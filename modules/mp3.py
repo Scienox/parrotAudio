1b16e647e7f684534e7489529411b5dd4ae60f27
Bex Jonathan <bexjonathan777@gmail.com>
2026-02-06 19:29:44 +0100

    help me

--- modules/mp3.py
+++ modules/mp3.py
@@ -125,8 +125,8 @@ class Mp3:
         cmd = message['message'].lower()
         
         try:
-            doubleCmd = True if (1 < len(cmd.split(':')) < 3) else False
-            tripleCmd = True if ':' in cmd and ':' in cmd.split(':')[1] else False
+            tripleCmd = len(cmd.split(':') == 3)
+            doubleCmd = len(cmd.split(':') == 2) and not tripleCmd
             if tripleCmd and doubleCmd:
                 doubleCmd = False
             if doubleCmd:
